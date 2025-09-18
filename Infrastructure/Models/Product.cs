a9bfa5c2a47cc662555832ba21b79fa168d6601e
osmanjeylani <osmanjeylani00@gmail.com>
2025-09-18 09:49:13 +0200

    Product Model created with properties.

---
+++ Infrastructure/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Models;
+
+public class Product
+{
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+}
