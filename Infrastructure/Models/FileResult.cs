5884a68a7ba2b90b46b0a9feffd7359d1d871594
osmanjeylani <osmanjeylani00@gmail.com>
2025-09-18 10:13:44 +0200

    IFileService and FileResult created.

---
+++ Infrastructure/Models/FileResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Models;
+
+public interface FileResult
+{
+    public bool Succeeded { get; set; }
+    public string? Error { get; set; }
+    public string? Content { get; set; }
+}
