95d7462fc4f50d9f0d2e43626d8579dbf624e9c8
osmanjeylani <osmanjeylani00@gmail.com>
2025-09-18 10:05:52 +0200

    IProductService and ProductResult model created

---
+++ Infrastructure/Models/ProductResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.Models;
+
+public class ProductResult
+{
+    public bool Succeeded { get; set; }
+    public string? Error { get; set; }
+    
+}
+
+public class ProductResult<T> : ProductResult
+{
+    public T? Content { get; set; }
+}
