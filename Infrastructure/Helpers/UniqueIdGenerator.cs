3fa61a7abe903d3bcff940514de61b35d7a481e7
osmanjeylani <osmanjeylani00@gmail.com>
2025-09-18 12:09:07 +0200

    Tests completed

---
+++ Infrastructure/Helpers/UniqueIdGenerator.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Interfaces;
+
+namespace Infrastructure.Helpers;
+
+public class UniqueIdGenerator : IUniqueIdGenerator
+{
+    public static string Generate()
+    {
+        return Guid.NewGuid().ToString();
+    }
+}
